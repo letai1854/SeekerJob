237ecab8e6dcebf55609fdfcd94ca1a4eb4dfc9a
leV9499 <133184770+leV9499@users.noreply.github.com>
2024-09-21 10:18:50 +0700

    [[]]

---
+++ SeekerJob/SeekerJob/Controllers/MenuController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SeekerJob.Controllers
+{
+    public class MenuController : Controller
+    {
+        // GET: Menu
+        MyDB db = new MyDB();
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+        public ActionResult GetMenu()
+        {
+            var tablemenus = db.tablemenus.Where(t => t.hide == true).OrderBy(t => t.arrange).ToList();
+
+
+            var tablemenuparts = db.tablemenuparts.Where(t => t.hide == true).OrderBy(t => t.arrange).ToList();
+
+
+            ViewData["tablemenus"] = tablemenus;
+            ViewData["tablemenuparts"] = tablemenuparts;
+
+            return PartialView("GetMenu");
+        }
+    }
+}
