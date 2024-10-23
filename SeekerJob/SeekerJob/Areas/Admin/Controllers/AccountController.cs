bfb32c7180413380863d1f0f6646b77caaa4decc
leV9499 <133184770+leV9499@users.noreply.github.com>
2024-10-23 20:50:25 +0700

    []

---
+++ SeekerJob/SeekerJob/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SeekerJob.Areas.Admin.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Admin/Account
+        mydatabase db = new mydatabase();
+        public ActionResult Index()
+        {
+            var table = db.Logins.ToList();
+
+            ViewData["table"] = table;
+            ViewBag.hoso = "ho-so-thi-sinh";
+            ViewBag.hosocongty = "ho-so-cong-ty";
+            return PartialView();
+        }
+    }
+}
---
+++ SeekerJob/SeekerJob/Areas/Admin/Controllers/BannerAndTitleController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SeekerJob.Areas.Admin.Controllers
+{
+    public class BannerAndTitleController : Controller
+    {
+        // GET: Admin/BannerAndTitle
+        mydatabase db = new mydatabase();
+        public ActionResult Index()
+        {
+            var banner = db.tablebanners
+                .Where(t => !string.IsNullOrEmpty(t.typeRow) && t.typeRow!="" && t.typeRow!=null &&t.typeRow!=EnumType.TypeTitleHome.headercompany.ToString()
+                         && t.typeRow != EnumType.Adertise.advertisehome2.ToString()
+                         && t.typeRow != EnumType.Adertise.advertisehome1.ToString()
+                         && t.typeRow != EnumType.Type.adminmange.ToString())
+                .OrderBy(t => t.id)
+                .ToList();
+            var titlepage = db.TitlePages.OrderBy(t=>t.datebegin).ToList();
+
+
+            var tablepannerpart = db.tablebannerparts.OrderBy(t=>t.id).ToList();
+
+            ViewData["banner"] = banner;
+            ViewData["tablepannerpart"] = tablepannerpart;
+            ViewData["titlepage"] = titlepage;
+            return PartialView();
+        }
+    }
+}
